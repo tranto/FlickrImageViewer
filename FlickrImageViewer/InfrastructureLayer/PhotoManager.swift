b8f2e376269289dc1f96821333f195fa5c49ff17
TranTo <totran0@gmail.com>
2020-08-09 21:42:03 +1000

    Photo Manager introduced

--- FlickrImageViewer/InfrastructureLayer/PhotoManager.swift
+++ FlickrImageViewer/InfrastructureLayer/PhotoManager.swift
@@ -5,5 +5,58 @@
 //  Created by Tran To on 9/8/20.
 //  Copyright © 2020 Tran To. All rights reserved.
 //
-
 import Foundation
+import Combine
+
+protocol PhotoManagerProtocol: class {
+	func searchPhotos(location: Location) -> AnyPublisher<Photos, InfrastructureError>
+	func searchPhotos(tags: [String], location: Location) -> AnyPublisher<Photos, InfrastructureError>
+}
+
+class PhotoManager: PhotoManagerProtocol {
+	
+	private let session: URLSession
+	private let config: ConfigurationManagerProtocol
+	private let requestFactory: RequestFactoryProtocol
+	init(session: URLSession = .shared, configurationManager: ConfigurationManagerProtocol) {
+		self.session = session
+		self.config = configurationManager
+		requestFactory = RequestFactory(config: config)
+	}
+	
+	func searchPhotos(location: Location) -> AnyPublisher<Photos, InfrastructureError> {
+		guard let components = try? requestFactory.makeSearchPhotosRequestWith(location: location),
+			let url = components.url else {
+				let error = InfrastructureError.client(description: "Cannot create url".localized)
+				return Fail(error: error).eraseToAnyPublisher()
+		}
+		
+		return session.dataTaskPublisher(for: URLRequest(url: url))
+			.mapError { error in
+				InfrastructureError.server(description: error.localizedDescription)
+		}
+		.flatMap(maxPublishers: .max(1)) { pair in
+			Parser.decode(pair.data)
+		}
+		.eraseToAnyPublisher()
+	}
+	
+	func searchPhotos(tags: [String], location: Location) -> AnyPublisher<Photos, InfrastructureError> {
+		
+		guard let components = try? requestFactory.makeSearchPhotosRequestWith(tags: tags, location: location),
+			let url = components.url else {
+				let error = InfrastructureError.client(description: "Cannot create url".localized)
+				return Fail(error: error).eraseToAnyPublisher()
+		}
+		
+		return session.dataTaskPublisher(for: URLRequest(url: url))
+			.mapError { error in
+				InfrastructureError.server(description: error.localizedDescription)
+		}
+		.flatMap(maxPublishers: .max(1)) { pair in
+			Parser.decode(pair.data)
+		}
+		.eraseToAnyPublisher()
+	}
+	
+}
